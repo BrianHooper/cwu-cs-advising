222c9cb2b6e0a5edd945c52cc8b51d7da89b5545
Brian Hooper <brian_hooper@msn.com>
2018-01-27 20:45:12 -0800

    Added basic js/jquery functionality to course and degree lists

---
+++ Code/Front_End/wwwroot/js/ListManager.js
@@ -0,0 +1,19 @@
+﻿
+
+$(document).ready(function () {
+    $(".ToggleElement").click(function () {
+        var x = "X";
+        var cur = $(this).html();
+
+        if ($(this).html() === "X") {
+            $(this).html("&nbsp;");
+        } else {
+            $(this).html("X");
+        }
+    });
+
+    $(".DeleteRow").click(function () {
+        $(this).parent().remove();
+    });
+});
+
