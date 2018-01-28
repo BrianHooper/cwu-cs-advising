2cdb5273cdfc67de39e25b9752bb1565c497d5ac
Brian Hooper <brian_hooper@msn.com>
2018-01-28 13:02:03 -0800

    Modified Course and Degree Requirement javascript

--- Code/Front_End/wwwroot/js/ListManager.js
+++ Code/Front_End/wwwroot/js/ListManager.js
@@ -1,7 +1,36 @@
-﻿
+﻿var CourseHtml = '<div class="Flex CenterFlex Row">' + 
+    '<div class="Element DeleteRow" >[X]</div >' +
+    '<div class="Element CourseElement"><input type="text" value="" /></div>' +
+    '<div class="Element CourseElement">' +
+    '<select>' +
+    '<option>------</option>' +
+    '<option>Computer Science</option>' +
+    '<option>Mathematics</option>' +
+    '</select>' +
+    '</div>' +
+    '<div class="Element CourseElement CourseNumberInput"><input type="text" value=""></div>' +
+    '<div class="Element CourseElement CourseNumberInput"><input type="text" value="0"></div>' +
+    '<div class="Element ToggleElement">&nbsp;</div>' +
+    '<div class="Element ToggleElement">&nbsp;</div>' +
+    '<div class="Element ToggleElement">&nbsp;</div>' +
+    '<div class="Element ToggleElement">&nbsp;</div>' +
+    '<div class="Element WideFlex">' +
+    '<a href="#">[ CS 361 ]</a>' +
+    '<a href="#">[ CS 312 ]</a>' +
+    '<a href="#">[+]</a>' +
+    '</div>' +
+    '</div>';
+
+var RequirementHtml = '<div class="Flex CenterFlex Row">' +
+    '<div class="Element DeleteRow">[X]</div>' +
+    '<div class="Element CourseElement"><input type="text" value="" /></div>' +
+    '<div class="Element WideFlex">' +
+    '<a href="#">[+]</a>' +
+    '</div>' +
+    '</div>';
 
 $(document).ready(function () {
-    $(".ToggleElement").click(function () {
+    $('#CourseContainer').on('click', '.ToggleElement', function () {
         var x = "X";
         var cur = $(this).html();
 
@@ -11,9 +40,22 @@ $(document).ready(function () {
             $(this).html("X");
         }
     });
+    $("#AddNewCourseButton").click(function () {
+        var CourseContainerHtml = $("#CourseContainer").html();
+        $("#CourseContainer").html(CourseContainerHtml + CourseHtml);
+    });
 
-    $(".DeleteRow").click(function () {
+    $('#CourseContainer').on('click', '.DeleteRow', function () {
         $(this).parent().remove();
+    });
+
+    $('#RequirementContainer').on('click', '.DeleteRow', function () {
+        $(this).parent().remove();
+    });
+
+    $("#AddNewRequirementButton").click(function () {
+        var ReqContainerHtml = $("#RequirementContainer").html();
+        $("#RequirementContainer").html(ReqContainerHtml + RequirementHtml);
     });
 });
 
