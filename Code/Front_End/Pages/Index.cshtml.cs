dc9209df04c28fd373b8dd18c9eed8f51e877358
Brian Hooper <brian_hooper@msn.com>
2018-01-29 22:59:05 -0800

    Added a dummy controller for the midterm presentation, and updated the base case.

--- Code/Front_End/Pages/Index.cshtml.cs
+++ Code/Front_End/Pages/Index.cshtml.cs
@@ -4,12 +4,45 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace CwuAdvising.Pages
 {
+
     public class IndexModel : PageModel
     {
         
+
+
+        public class StudentInfoModel
+        {
+            public string Name { get; set; }
+            public string ID { get; set; }
+            public string Quarter { get; set; }
+            public string Year { get; set; }
+            public string Degree { get; set; }
+        }
+
+        [BindProperty]
+        public StudentInfoModel Index { get; set; }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            DemoController.StudentName = Index.Name;
+            DemoController.StudentID = Index.ID;
+            DemoController.StudentQuarter = Index.Quarter;
+            DemoController.StudentYear = Index.Year;
+            DemoController.StudentDegree = Index.Degree;
+
+
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            // create and send the mail here
+            return RedirectToPage("Advising");
+        }
         public void OnGet()
         {
 
