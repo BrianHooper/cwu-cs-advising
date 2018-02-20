75dd95f40b4ff20a9a95d323bbf9db8163ad3478
ricohelvid <33173288+ricohelvid@users.noreply.github.com>
2018-02-19 23:09:37 -0800

    Add files via upload
    tried to implement some basic branch and bound. these are just initial classes which might be needed.

---
+++ Code/Back_End/PlanGenerationAlgorithm/Graph.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PlanGenerationAlgorithm
+{
+    class Graph
+    {
+        public Node Root;
+        public List<Node> AllNodes = new List<Node>();
+
+        public Node CreateRoot(string name)
+        {
+            Root = CreateNode(name);
+            return Root;
+        }
+
+        public Node CreateNode(string name)
+        {
+            var n = new Node(name);
+            AllNodes.Add(n);
+            return n;
+        }
+    }
+}
---
+++ Code/Back_End/PlanGenerationAlgorithm/Node.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PlanGenerationAlgorithm
+{
+    public class Arc
+    {
+        public int weight;
+        public Node Parent;
+        public Node Child;
+    }
+    public class Node
+    {
+        public string Name;
+        public List<Arc> Arcs = new List<Arc>();
+        public int profit;
+        public int weight;
+        public int level;
+        public Node(string name)
+        {
+            Name = name;
+        }
+
+        /// <summary>
+        /// Create a new arc, connecting this Node to the Nod passed in the parameter
+        /// Also, it creates the inversed node in the passed node
+        /// </summary>
+        public Node AddArc(Node child, int w)
+        {
+            Arcs.Add(new Arc
+            {
+                Parent = this,
+                Child = child,
+                weight = w
+            });
+
+            if (!child.Arcs.Exists(a => a.Parent == child && a.Child == this))
+            {
+                child.AddArc(this, w);
+            }
+
+            return this;
+        }
+    }
+}
