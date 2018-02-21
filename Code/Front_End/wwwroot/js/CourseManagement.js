bdcfb14784bad25ba214b13bff227dde5a2a04f4
Brian Hooper <brian_hooper@msn.com>
2018-02-21 01:47:56 -0800

    Finished search functionality in Course Management page.

--- Code/Front_End/wwwroot/js/CourseManagement.js
+++ Code/Front_End/wwwroot/js/CourseManagement.js
@@ -2,47 +2,122 @@
 var checkIcon = "&#10004;";
 var editIcon = "&#9998;";
 
-var Departments = ["Computer Science", "Mathematics"];
-var StartingCourses = '[{"Title":"Computer Architecture II","Department":"Computer Science","Number":"312","Credits":"4","Offered":"32","PreReqs":["CS 301"]},{"Title":"Data Structures","Department":"Computer Science","Number":"302","Credits":"4","Offered":"4","PreReqs":[]},{"Title":"Operating Systems","Department":"Computer Science","Number":"470","Credits":"4","Offered":"1","PreReqs":["CS 361"]}]';
+var Departments = [];
+var StartingCourses = '[{"Title":"Discrete Math","Department":"Mathematics","Number":"330","Credits":"5","Offered":"32","PreReqs":["CS 301"]},{"Title":"Data Structures","Department":"Computer Science","Number":"302","Credits":"4","Offered":"4","PreReqs":[]},{"Title":"Operating Systems","Department":"Computer Science","Number":"470","Credits":"4","Offered":"1","PreReqs":["CS 361"]}]';
+var CourseList = JSON.parse(StartingCourses);
 
 $(document).ready(function () {
-    LoadCourses();
+    ResetCourseContainer();
+    LoadDepartments(CourseList);
+    //LoadCourses(CourseList);
 });
 
-function LoadCourses() {
-    var Courses = JSON.parse(StartingCourses);
+$(document).on('click', '#SearchCourses', function () {
+    if (!SaveCourses()) {
+        return;
+    }
+    ResetCourseContainer();
 
+    var MinCourseNumber = $("#MinCourseSearch").val();
+    var MaxCourseNumber = $("#MaxCourseSearch").val();
+    var Department = $("#DepartmentSearch").val();
+    var OfferedSummer = $("#SummerSearch").is(":checked");
+    var OfferedFall = $("#FallSearch").is(":checked");
+    var OfferedWinter = $("#WinterSearch").is(":checked");
+    var OfferedSpring = $("#SpringSearch").is(":checked");
+
+    for (var i = 0; i < CourseList.length; i++) {
+        var MatchesCourse = false;
+        
+        if ((OfferedSummer && StringMatch(CourseList[i].Offered, "3"))) {
+            MatchesCourse = true;
+        } else if ((OfferedFall && StringMatch(CourseList[i].Offered, "4"))) {
+            MatchesCourse = true;
+        } else if ((OfferedWinter && StringMatch(CourseList[i].Offered, "1"))) {
+            MatchesCourse = true;
+        } else if ((OfferedSpring && StringMatch(CourseList[i].Offered, "2"))) {
+            MatchesCourse = true;
+        }
+
+        if (!StringMatch(Department, "any") && !StringMatch(CourseList[i].Department, Department)) {
+            MatchesCourse = false;
+        } 
+
+        if (CourseList[i].Number < MinCourseNumber || CourseList[i].Number > MaxCourseNumber) {
+            MatchesCourse = false;
+        }
+        
+        if (MatchesCourse) {
+            LoadCourse(CourseList[i]);
+        }
+    }
+});
+
+function SaveCourses() {
+    var Courses = ReadCoursesToList();
+    return true;
+}
+
+function ResetCourseContainer() {
+    $("#CourseContainer").html("");
+    $("#CourseContainer").append($("<div class='Title'>Courses</div>"));
+    var CourseHeader = $("<div></div>");
+    CourseHeader.attr("class", "Flex CenterFlex Row skip");
+    CourseHeader.append($('<div class="Element ButtonElement">' + deleteIcon + '</div>'));
+    CourseHeader.append($('<div class="Element CourseElement">Course Title</div>'));
+    CourseHeader.append($('<div class="Element CourseElement">Department</div>'));
+    CourseHeader.append($('<div class="Element CourseElement CourseNumberInput">Number</div>'));
+    CourseHeader.append($('<div class="Element CourseElement CourseNumberInput">Credits</div>'));
+    CourseHeader.append($('<div class="Element ButtonElement">Su</div>'));
+    CourseHeader.append($('<div class="Element ButtonElement">F</div>'));
+    CourseHeader.append($('<div class="Element ButtonElement">W</div>'));
+    CourseHeader.append($('<div class="Element ButtonElement">S</div>'));
+    CourseHeader.append($('<div class="Element WideFlex">Prereqs</div>'));
+    $("#CourseContainer").append(CourseHeader);
+}
+
+function LoadDepartments(Courses) {
+    Departments.push("");
     for (var i = 0; i < Courses.length; i++) {
-        //console.log(Courses[i].Title);
-        var CourseRow = CreateCourseRow();
-        CourseRow.children().eq(1).children().first().val(Courses[i].Title);
-
         if (!StringArrayContains(Departments, Courses[i].Department)) {
             Departments.push(Courses[i].Department);
+            $("#DepartmentSearch").append("<option value='" + Courses[i].Department + "'>" + Courses[i].Department + "</option>");
         }
-        CourseRow.children().eq(2).children().first().append($("<option value='" + Courses[i].Department + "'>" + Courses[i].Department + "</option>"));
-        CourseRow.children().eq(3).children().first().val(Courses[i].Number);
-        CourseRow.children().eq(4).children().first().val(Courses[i].Credits);
-
-        if (StringMatch(Courses[i].Offered, "3")) {
-            ToggleClick(CourseRow.children().eq(5));
-        }
-        if (StringMatch(Courses[i].Offered, "4")) {
-            ToggleClick(CourseRow.children().eq(6));
-        }
-        if (StringMatch(Courses[i].Offered, "1")) {
-            ToggleClick(CourseRow.children().eq(7));
-        }
-        if (StringMatch(Courses[i].Offered, "2")) {
-            ToggleClick(CourseRow.children().eq(8));
-        }
-
-        for (var j = 0; j < Courses[i].PreReqs.length; j++) {
-            CourseRow.children().eq(9).append($("<span class='Prereq'>" + Courses[i].PreReqs[j] + "</span>"));
-        }
-
-        $("#CourseContainer").append(CourseRow);
     }
+}
+
+function LoadCourses(Courses) {
+    for (var i = 0; i < Courses.length; i++) {
+        LoadCourse(Courses[i]);
+    }
+}
+
+function LoadCourse(Course) {
+    var CourseRow = CreateCourseRow();
+    CourseRow.children().eq(1).children().first().val(Course.Title);
+
+    CourseRow.children().eq(2).children().first().val(Course.Department);
+    CourseRow.children().eq(3).children().first().val(Course.Number);
+    CourseRow.children().eq(4).children().first().val(Course.Credits);
+
+    if (StringMatch(Course.Offered, "3")) {
+        ToggleClick(CourseRow.children().eq(5));
+    }
+    if (StringMatch(Course.Offered, "4")) {
+        ToggleClick(CourseRow.children().eq(6));
+    }
+    if (StringMatch(Course.Offered, "1")) {
+        ToggleClick(CourseRow.children().eq(7));
+    }
+    if (StringMatch(Course.Offered, "2")) {
+        ToggleClick(CourseRow.children().eq(8));
+    }
+
+    for (var j = 0; j < Course.PreReqs.length; j++) {
+        CourseRow.children().eq(9).append($("<span class='Prereq'>" + Course.PreReqs[j] + "</span>"));
+    }
+
+    $("#CourseContainer").append(CourseRow);
 }
 
 function CreateCourseRow() {
@@ -67,11 +142,11 @@ function CreateCourseRow() {
     var DepartmentBox = $("<div></div>");
     DepartmentBox.attr("class", "Element CourseElement");
     var DepartmentSelect = $("<select></select>");
-    /*
+    
     for (var i = 0; i < Departments.length; i++) {
         DepartmentSelect.append($("<option value='" + Departments[i] + "'>" + Departments[i] + "</option>"));
     }
-    */
+    
     DepartmentBox.append(DepartmentSelect);
     RowDiv.append(DepartmentBox);
 
@@ -146,17 +221,23 @@ function ToggleClick(Element) {
 
 /*  Builds the Json object to pass back to the server   */
 $(document).on("click", "#SaveCourses", function () {
+    var Courses = ReadCoursesToList();
+    var ModifiedCoursesJson = JSON.stringify(Courses);
+    console.log(ModifiedCoursesJson);
+});
+
+function ReadCoursesToList() {
     var Courses = [];
     $(".Row").each(function () {
         if (!$(this).hasClass("skip")) {
-            if (HasAttrValue($(this), "modified", "true")) { 
+            if (HasAttrValue($(this), "modified", "true")) {
                 var Course = { "Title": "", "Department": "", "Number": "", "Credits": "", "Offered": "", "PreReqs": [] };
                 var Row = $(this).children();
                 Course.Title = Row.eq(1).children().first().val();
                 Course.Department = Row.eq(2).children().first().val();
                 Course.Number = Row.eq(3).children().first().val();
                 Course.Credits = Row.eq(4).children().first().val();
-                
+
                 var Offered = "";
                 if (HasAttrValue(Row.eq(5), "toggled", "true")) Offered += "3";
                 if (HasAttrValue(Row.eq(6), "toggled", "true")) Offered += "4";
@@ -172,9 +253,8 @@ $(document).on("click", "#SaveCourses", function () {
             }
         }
     });
-    var ModifiedCoursesJson = JSON.stringify(Courses);
-    console.log(ModifiedCoursesJson);
-});
+    return Courses;
+}
 
 $(document).on("click", ".PrereqBox", function () {
     var DialogBox = $("<div></div>");
