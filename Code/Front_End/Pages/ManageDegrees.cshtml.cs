2faaf45d6010e8ab6919eb1f87573e401d49ada7
Brian Hooper <brian_hooper@msn.com>
2018-02-28 08:54:07 -0800

    Updated DegreeManagement to use new database connection

--- Code/Front_End/Pages/ManageDegrees.cshtml.cs
+++ Code/Front_End/Pages/ManageDegrees.cshtml.cs
@@ -14,22 +14,54 @@ namespace CwuAdvising.Pages
     /// <summary>Model for degree management</summary>
     public class ManageDegreesModel : PageModel
     {
+        /// <summary>List containing the degrees loaded from the database</summary>
+        public static List<DegreeModel> DegreeList { get; set; } = null;
+
         /// <summary>Loads degrees from the database</summary>
         /// <returns>Returns a list of DegreeModels serialized as a JSON string</returns>
-        public static string LoadDatabaseDegrees()
+        public static bool LoadDatabaseDegrees()
         {
-            List<DegreeModel> model = new List<DegreeModel>();
-            
-            DegreeModel DM = new DegreeModel("BS - Computer Science", 2018);
-            DM.requirements.Add("CS301");
-            DM.requirements.Add("CS302");
-            DM.requirements.Add("CS361");
-            DM.requirements.Add("CS470");
-            model.Add(DM);
-            model.Add(new DegreeModel("BS - Information Technology", 2018));
-            model.Add(new DegreeModel("BS - Mathematics", 2018));
-            
-            return JsonConvert.SerializeObject(model);
+            DegreeList = new List<DegreeModel>();
+
+            if (Program.Database.connected)
+            {
+                // Load degrees from database
+                return true;
+            }
+            else
+            {
+                DegreeModel DM = new DegreeModel("BS - Computer Science", 2018);
+                DM.requirements.Add("CS301");
+                DM.requirements.Add("CS302");
+                DM.requirements.Add("CS361");
+                DM.requirements.Add("CS470");
+                DegreeList.Add(DM);
+                DegreeList.Add(new DegreeModel("BS - Information Technology", 2018));
+                DegreeList.Add(new DegreeModel("BS - Mathematics", 2018));
+                return false;
+            }
+        }
+
+        /// <summary>Parses the degree list</summary>
+        /// <returns>DegreeModel list as a JSON string</returns>
+        public static string ReadModel()
+        {
+            if(DegreeList != null)
+            {
+                return JsonConvert.SerializeObject(DegreeList);
+            }
+            else
+            {
+                return "";
+            }
+        }
+
+        /// <summary>Updates database with the modified degree</summary>
+        /// <param name="model">Model of the degree to be updated</param>
+        /// <returns>true if the update was successful</returns>
+        public static bool WriteDatabaseDegree(DegreeModel model)
+        {
+            return false;
         }
 
         /// <summary>Retrieves a modified degree as JSON data from POST</summary>
