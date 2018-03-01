a14c125f929ead69b6396554f2509c7c6cfdf6ff
Brian Hooper <brian_hooper@msn.com>
2018-02-28 23:18:59 -0800

    Added POST, validation, and error messages to login form

--- Code/Front_End/Pages/Index.cshtml.cs
+++ Code/Front_End/Pages/Index.cshtml.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
+using Database_Object_Classes;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.AspNetCore.Mvc.Rendering;
@@ -11,6 +13,56 @@ namespace CwuAdvising.Pages
     /// <summary>Model for index page</summary>
     public class IndexModel : PageModel
     {
+        /// <summary>Error message to display to the user on failed login</summary>
+        public static string LoginErrorMessage = "";
+
+        /// <summary></summary>
+        public class LoginModel
+        {
+            /// <summary>Username for login</summary>
+            [Required]
+            public string Username { get; set; }
+
+            /// <summary>Password for login</summary>
+            [Required]
+            public string Password { get; set; }
+        }
+
+        /// <summary>Binds the LoginModel to a Login object for POST</summary>
+        [BindProperty]
+        public LoginModel Login { get; set; }
         
+        /// <summary>Recieve login information</summary>
+        /// <returns>Redirects to StudentAdvising if logging in is successful</returns>
+        public IActionResult OnPost()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page(); // Form validation failed
+            }
+            
+            if(!Program.Database.connected)
+            {
+                LoginErrorMessage = "Error, database connection failed.";
+                return Page();
+            }
+
+            string username = Login.Password;
+            string password = Login.Username;
+            // Access database
+
+            bool loggedIn = true;
+            if (loggedIn) // Login successful
+            {
+                return RedirectToPage("StudentAdvising");
+            }
+            else
+            {
+                LoginErrorMessage = "Error, invalid username or password.";
+                return Page();
+            }
+        }
+
+
     }
 }
